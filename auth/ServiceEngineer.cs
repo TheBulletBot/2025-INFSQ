f5e9859a83fdfdfc5d3b360161e0e40542335319
Slinck0 <nickstorm2005@gmail.com>
2025-06-18 21:26:41 +0200

    Ik heb alles overgezet en systemadmin aangemaakt

---
+++ auth/ServiceEngineer.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class ServiceEngineer : User
+{
+    // aanpassen tot de database maar ik heb geen idee (:
+    public ServiceEngineer(string username, string role) : base(username, role)
+    {
+     
+    }
+
+    public override void Menu()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine($"=== Service Engineer Menu ({Username}) ===");
+            Console.WriteLine("1. Change password");
+            Console.WriteLine("2. Edit scooter attributes");
+            Console.WriteLine("3. Search scooter");
+            Console.WriteLine("4. Return");
+
+            Console.Write("Pick an option: ");
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    ChangePassword();
+                    break;
+                case "2":
+                    ChangeScooter();
+                    break;
+                case "3":
+                    SearchScooter();
+                    break;
+                case "4":
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+
+    private void ChangePassword()
+    {
+        Console.Write("Enter your new password: ");
+        string password = Console.ReadLine();
+        Console.WriteLine("Password succesfully changed.");
+        Console.WriteLine("Hit any key to continue");
+        Console.ReadKey();
+    }
+
+    private void ChangeScooter()
+    {
+        
+    }
+
+    private void SearchScooter()
+    {
+        
+    }
+}
--- auth/User.cs
+++ auth/User.cs
@@ -11,39 +11,7 @@ public abstract class User
     public abstract void Menu();
 }
 
-public class ServiceEngineer : User
-{
-    public ServiceEngineer(string username, string role) : base(username, role)
-    {
-
-    }
-    //Insert Menus for Service Engineers here.
-    public override void Menu()
-    {
-        
-    }
-    //Insert all functions that Service Engineers can perform here. 
-
-
-
-}
-
-public class Admin : ServiceEngineer
-{
-    public Admin(string username, string role) : base(username, role)
-    {
-
-    }
-    //Insert Menus for Admins here
-    public override void Menu()
-    {
-
-    }
-
-    //Insert all functions that Only Admins can perform here
-}
-
-public class SuperAdmin : Admin
+public class SuperAdmin : SystemAdmin
 {
     public SuperAdmin(string username, string role) : base(username, role)
     {
