5dd34c553d00d51b70011eb7cbbef620e214fde6
thebulletbot <41111591+TheBulletBot@users.noreply.github.com>
2025-06-18 16:18:26 +0200

    Added Regex class with default Regex declarations (add more where necessary)

--- Login.cs
+++ Login.cs
@@ -1,7 +1,16 @@
 public static class Login
 {
+
     public static void LoginScreen()
     {
+        System.Console.WriteLine("Welcome to ScooterVille's Scooter Rental Management System\n \nPlease Log in.\n\n\n");
+        System.Console.WriteLine("Username: ");
+        var username = Console.ReadLine();
+        System.Console.WriteLine("\nPassword: ");
+        var password = HashCode.Combine(Console.ReadLine());
 
+        System.Console.WriteLine($"DEBUG: Username = {username}; PasswordHash = {password}");
+
+        //fetch User with username = 
     }
 }
